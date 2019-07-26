20a2c5c61e3eed46f58af5d287ee1f6743f0c449
Zhang Yungui <rhcad@hotmail.com>
2019-07-26 15:47:07 +0800

    完善cbeta_build.py，增加栏列号，待识别卷

--- data/cbeta_build.py
+++ data/cbeta_build.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# nohup python3 /home/sm/cbeta/code/controller/data/cbeta_build.py >> /home/sm/cbeta/cbeta.log 2>&1 &
-# python3 controller/data/cbeta_build.py --bm_path=BM_u8_path
+# nohup python3 /home/sm/cbeta/code/data/cbeta_build.py >> /home/sm/cbeta/cbeta.log 2>&1 &
+# python3 data/cbeta_build.py --bm_path=BM_u8_path
+# 执行前，需要将 cbeta-juan、cbeta-mulu 放到本文件的目录下。
 #
 # 查看实际导入的数量: curl 'localhost:9200/_cat/indices?v'
 # 查看最近导入的日志: python3 -c "print(''.join(open('/home/sm/cbeta/cbeta.log').readlines()[-5:]))"
@@ -14,17 +15,18 @@ from glob2 import glob
 from datetime import datetime
 from functools import partial
 
-sys.path.append(path.dirname(path.dirname(path.dirname(__file__))))  # to use controller
+sys.path.append(path.dirname(path.dirname(__file__)))  # 为了使用下列controller模块
 
+from controller.app import Application
 from controller.data.variant import normalize
 from controller.data.rare import format_rare
 from elasticsearch import Elasticsearch
 from elasticsearch.exceptions import ElasticsearchException
-from controller.app import Application
 
 config = Application.load_config()['esearch']
 BM_PATH = config.get('BM_u8') or '/home/sm/cbeta/BM_u8'
-re_head_parts = re.compile(r'^([A-Z]{1,2})(\d+)n([A-Z]?\d+)([A-Za-z_]?)p([a-z]?\d+)')
+re_head_parts = re.compile(r'^([A-Z]{1,2})(\d+)n([A-Z]?\d+)([A-Za-z_]?)p([a-z]?\d+)([a-z]\d+)?')
+output = dict(pages=[])
 
 
 def junk_filter(txt):
@@ -38,7 +40,7 @@ def cur_time():
     return datetime.now().strftime('[%H:%M:%S]')
 
 
-def add_page(index, rows, page_code, line=0):
+def add_page(index, rows, page_code, cols=None, line=0):
     if rows:
         origin = [format_rare(r) for r in rows]
         normal = [normalize(r) for r in origin]
@@ -63,21 +65,30 @@ def add_page(index, rows, page_code, line=0):
             sutra_no: 经号，例如 1057、A042
             sutra_code: 典籍编号，藏经代码+经号，例如 A1057
             page_no: 页号，例如 0319、b005
+            cols: {栏列号: [起始文本行序号, 终止文本行序号], 'c02': [12, 49]}
             origin: 原始文本，部分组字式已替换为生僻字
             normal: 规范文本，是对原始文本的异体字转换为规范字的结果
             lines: 文本行数
             char_count: 规范文本(含标点)的字数
             updated_time: 入库时间
             '''
-            index(body=dict(
-                page_code=page_code, canon_code=canon_code, book_no=book_no, book_code=book_code,
-                sutra_no=sutra_no, sutra_code=sutra_code, edition=edition, page_no=page_no,
-                origin=origin, normal=normal, lines=len(rows), char_count=count, updated_time=datetime.now())
-            )
-            # print('%s\tsuccess:\t%s\t%s lines\t %s chars' % (cur_time(), page_code, len(rows), len(origin)))
-            if line > 0:
-                sys.stdout.write('%d %s,' % (line, page_code[len(book_code):]))
-            elif line < 0:
+            if index is not None:
+                index(body=dict(
+                    page_code=page_code, canon_code=canon_code, book_no=book_no, book_code=book_code,
+                    sutra_no=sutra_no, sutra_code=sutra_code, edition=edition, page_no=page_no, cols=cols,
+                    origin=origin, normal=normal, lines=len(rows), char_count=count, updated_time=datetime.now())
+                )
+            else:
+                output['pages'].append(dict(page_code=page_code))
+                if line < 0:
+                    codes = [p['page_code'] for p in output['pages']]
+                    with open('build.log', 'a') as f:
+                        f.write('%s %d pages\n%s\n' % (
+                            book_code, len(output['pages']), ', '.join(codes)))
+                    output['pages'] = []
+            if line > 0 and 0:
+                sys.stdout.write('%d %s, ' % (line, page_code[len(book_code):]))
+            elif line < 0 and 0:
                 sys.stdout.write(page_code + '\n')
             return True
         except ElasticsearchException as e:
@@ -85,32 +96,49 @@ def add_page(index, rows, page_code, line=0):
             return False
 
 
-def scan_and_index_dir(index, source):
+def scan_and_index_dir(index, source, book_code):
     """直接导入目录中的文本数据"""
     errors = []
     for i, fn in enumerate(sorted(glob(path.join(source, '**', r'new.txt')))):
-        print('%s processing file %s' % (cur_time(), fn))
+        if book_code and '/%s/' % book_code not in fn:
+            continue
+        sys.stdout.write('%s processing %s ' % (cur_time(), fn))  # eg: BM_u8/G/G083/new.txt
         with open(fn, 'r', encoding='utf-8') as f:
             lines = f.readlines()
         rows, page_code = [], None
-        count = 0
-        for line, row in enumerate(lines):
+        cols = {}
+        count = idx = total = 0
+        for row in lines:
             texts = re.split('#{1,3}', row.strip(), 1)
             if len(texts) != 2:
                 continue
             head = re_head_parts.search(texts[0])
+            col_line = head and head.group(6)
             if head:
-                if page_code and page_code != head.group(0):
+                if page_code and page_code not in head.group(0):
                     count += 1
-                    if not add_page(index, rows, page_code, count):
+                    total += len(rows)
+                    if not add_page(index, rows, page_code, cols, count):
                         errors.append(page_code)
                     rows = []
-                page_code = head.group(0)
+                    idx = 0
+                page_code = head.group(0)[:-len(col_line)]
             else:
                 print('head error:\t%s' % row)
             rows.append(junk_filter(texts[1]))
-        if not add_page(index, rows, page_code, -1):
+            col_code, line = col_line[0], col_line[1:]
+            if col_code not in cols:
+                cols[col_code] = [col_line, idx, col_line, idx]
+            else:
+                cols[col_code][2] = col_line
+                cols[col_code][3] = idx
+            idx += 1
+        if not add_page(index, rows, page_code, cols, -1):
             errors.append(page_code)
+        elif rows:
+            count += 1
+            total += len(rows)
+        sys.stdout.write('%d pages, %d lines\n' % (count, total))
 
     if errors:
         with open(path.join(source, datetime.now().strftime('error-%Y%m%d-%H%M.json')), 'w') as f:
@@ -118,37 +146,17 @@ def scan_and_index_dir(index, source):
         print('%s error pages\n%s' % (len(errors), errors))
 
 
-def index_page_codes(index, fn, base_dir):
-    """根据json文件中指定的page_code页码重新导入"""
-    with open(fn, 'r', encoding='utf-8') as f:
-        page_codes = json.load(f)
-    errors = []
-    for page_code in page_codes:
-        head = re.search(r'^([A-Z]{1,2})(\d+)n([A-Z]?\d+)[A-Za-z_]?p([a-z]?\d+)', page_code)
-        from_file = path.join(base_dir, head.group(1), head.group(1) + head.group(2), 'new.txt')
-        with open(from_file, 'r', encoding='utf-8') as f:
-            lines = f.readlines()
-        rows = [junk_filter(re.split('#{1,3}', line.strip(), 1)[1]) for line in lines if page_code in line]
-        if not add_page(index, rows, page_code):
-            errors.append(page_code)
-
-    if errors:
-        with open(path.join(path.basename(fn), datetime.now().strftime('error-%Y%m%d-%H%M.json'))) as f:
-            json.dump(errors, f)
-        print('%s error pages\n%s' % (len(errors), errors))
-
-
-def build_db(index='cb4ocr-ik', source='default', bm_path=BM_PATH, mode='create', split='ik'):
+def build_db(index='cb4ocr-ik', bm_path=BM_PATH, mode='create', book_code='', split='ik'):
     """ 基于CBETA文本创建索引，以便ocr寻找比对文本使用
     :param index: 索引名称
-    :param source: 待加工的数据来源，有两种：
-            1.目录(default)：这种情况下直接导入目录中的文本数据
-            2.json文件名：这种情况下根据json文件中指定的page_code页码重新导入，txt目录可在bm_path参数指定
     :param bm_path: BM_u8文本目录
     :param mode: 'create'表示新建，'update'表示更新
+    :param book_code: 仅导入指定册别的页面
     :param split: 中文分词器的名称，如'ik'或'jieba'
     """
-    es = Elasticsearch()
+    es = index and Elasticsearch()
+    if not es:
+        return scan_and_index_dir(None, bm_path, book_code)
 
     if mode == 'create':
         es.indices.delete(index=index, ignore=[400, 404])
@@ -169,10 +177,7 @@ def build_db(index='cb4ocr-ik', source='default', bm_path=BM_PATH, mode='create'
         }}
         es.indices.put_mapping(index=index, body=mapping)
 
-    if '.json' in source:
-        index_page_codes(partial(es.index, index=index, ignore=[]), source, bm_path)
-    else:
-        scan_and_index_dir(partial(es.index, index=index, ignore=[]), bm_path)
+    scan_and_index_dir(partial(es.index, index=index, ignore=[]), bm_path, book_code)
 
 
 if __name__ == '__main__':
