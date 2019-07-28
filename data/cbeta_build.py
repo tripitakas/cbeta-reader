70dd31630a3eb12d8bcd7a80b357ace881279ee6
Zhang Yungui <rhcad@hotmail.com>
2019-07-28 21:35:43 +0800

    加页面数据字段说明，对没有卷的自动识别J标签

--- data/cbeta_build.py
+++ data/cbeta_build.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # nohup python3 /home/sm/cbeta/code/data/cbeta_build.py >> /home/sm/cbeta/cbeta.log 2>&1 &
 # python3 data/cbeta_build.py --bm_path=BM_u8_path
@@ -6,6 +6,7 @@
 #
 # 查看实际导入的数量: curl 'localhost:9200/_cat/indices?v'
 # 查看最近导入的日志: python3 -c "print(''.join(open('/home/sm/cbeta/cbeta.log').readlines()[-5:]))"
+# 停止导入任务: kill -9 `ps -ef | grep cbeta_build.py | grep -v grep | awk -F" " {'print $2'}` 2
 
 import re
 import sys
@@ -18,8 +19,8 @@ from functools import partial
 sys.path.append(path.dirname(path.dirname(__file__)))  # 为了使用下列controller模块
 
 from controller.app import Application
-from controller.data.variant import normalize
-from controller.data.rare import format_rare
+from controller.cbeta.variant import normalize
+from controller.cbeta.rare import format_rare
 from elasticsearch import Elasticsearch
 from elasticsearch.exceptions import ElasticsearchException
 
@@ -67,7 +68,8 @@ def add_page(index, rows, page_code, cols, juan, line=0):
             sutra_no: 经号，例如 1057、A042
             sutra_code: 典籍编号，藏经代码+经号，例如 A1057
             page_no: 页号，例如 0319、b005
-            cols: {栏列号: [起始文本行序号, 终止文本行序号], 'c02': [12, 49]}
+            cols: [[栏号, 起始栏列号, 起始文本行序号, 终止栏列号, 终止文本行序号]]，例如：
+                  [["a", "a01", 0, "a26", 25], ["b", "b01", 26, "b26", 51]]
             juan: 本页的卷项数组[{"n": "001", "fun": "open", "head": "T03n0152_p0001a03", "title": "六度集經卷第一"},]
             origin: 原始文本，部分组字式已替换为生僻字
             normal: 规范文本，是对原始文本的异体字转换为规范字的结果
@@ -83,6 +85,7 @@ def add_page(index, rows, page_code, cols, juan, line=0):
                 )
             else:
                 output['pages'].append(dict(page_code=page_code, juan=juan, cols=cols,
+                                            origin=[origin[0], origin[-1]],
                                             lines=len(rows), char_count=count))
                 if line < 0:
                     codes = [p['page_code'] for p in output['pages']]
@@ -153,6 +156,9 @@ def scan_and_index_dir(index, source, book_code):
             rows.append(junk_filter(texts[1]))
             juan_ = head.group(0)
             juan_ = [p for p in juan_list if p['head'] == juan_]
+            if not juan_ and not (juan_opened and juan_opened[0]['fun'] == 'opened') and head:
+                if '<J>' in texts[1] or texts[0].endswith('J'):
+                    juan_ = [dict(fun='J', head=head.group(0), title=rows[-1])]
             if juan_:
                 juan_ = juan_[0]
                 juan.append(juan_)
@@ -185,7 +191,7 @@ def build_db(index='cb4ocr-ik', bm_path=BM_PATH, mode='create', book_code='', sp
     :param book_code: 仅导入指定册别的页面
     :param split: 中文分词器的名称，如'ik'或'jieba'
     """
-    es = index and Elasticsearch()
+    es = 0 and index and Elasticsearch()
     if not es:
         return scan_and_index_dir(None, bm_path, book_code)
 
