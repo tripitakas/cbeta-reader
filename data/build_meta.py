ef3b3834e21ef9aa6430e3301b17606ada54a3d8
Zhang Yungui <rhcad@hotmail.com>
2019-07-30 17:00:23 +0800

    去掉容易混淆的文件

--- data/build_meta.py
+++ data/build_meta.py
@@ -1,144 +1,196 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# nohup python3 /home/sm/cbeta/code/data/build_meta.py >> /home/sm/cbeta/cbeta.log 2>&1 &
+# python3 data/cbeta_build.py --bm_path=BM_u8_path
+# 执行前，需要将 cbeta-juan、cbeta-mulu 放到本文件的目录下。
+#
+# 查看实际导入的数量: curl 'localhost:9200/_cat/indices?v'
+# 查看最近导入的日志: python3 -c "print(''.join(open('/home/sm/cbeta/cbeta.log').readlines()[-5:]))"
+# 停止导入任务: sudo kill -9 `ps -ef | grep cbeta_build.py | grep -v grep | awk -F" " {'print $2'}` 2
 
-import os
 import re
+import sys
 import json
+from os import path
 from glob2 import glob
-import os.path as path
-from lxml import etree
 from datetime import datetime
-from functools import cmp_to_key
+from functools import partial
 
-XML_P5_DIR = './xml/xml-p5'
-MULU_DIR = './meta/mulu'
-JUAN_DIR = './meta/juan'
+sys.path.append(path.dirname(path.dirname(__file__)))  # 为了使用下列controller模块
+
+from controller.app import Application
+from controller.cbeta.variant import normalize
+from controller.cbeta.rare import format_rare
+from controller.cbeta.meta import get_juan
+from elasticsearch import Elasticsearch
+from elasticsearch.exceptions import ElasticsearchException
+
+config = Application.load_config()['esearch']
+BM_PATH = config.get('BM_u8') or '/home/sm/cbeta/BM_u8'
+juan_path = path.join(path.dirname(__file__), 'cbeta-juan')
+re_head_parts = re.compile(r'^([A-Z]{1,2})(\d+)n([A-Z]?\d+)([A-Za-z_]?)p([a-z]?\d+)([a-z]\d+)?')
+output = dict(pages=[])
 
 
-def extract_mulu_from_xml_p5(source=XML_P5_DIR, overwrite=False):
-    """ 从CBETA xml-p5中提取目录信息 """
-    for fn in glob(path.join(source, '**', '*.xml')):
-        print('[%s]%s: processing... ' % ((datetime.now().strftime('%Y-%m-%d %H:%M:%S')), path.basename(fn)))
-        # 检查目录信息是否已存储
-        head = re.search(r'^([A-Z]{1,2})(\d+)n(.*)', path.basename(fn))
-        assert head and head.group()
-        to_dir = path.join(MULU_DIR, head.group(1), head.group(1) + head.group(2))
-        if not path.exists(to_dir):
-            os.makedirs(to_dir)
-        to_file = path.join(to_dir, path.splitext(path.basename(fn))[0] + '.json')
-        if not overwrite and path.exists(to_file):
-            print('[%s]%s: mulu file existed! ' % ((datetime.now().strftime('%Y-%m-%d %H:%M:%S')), path.basename(fn)))
-            continue
+def junk_filter(txt):
+    txt = re.sub('<[^>]*>', '', txt)
+    txt = re.sub(r'\[.>(.)\]', lambda m: m.group(1), txt)
+    txt = re.sub(r'\[[\x00-\xff＊]*\]', '', txt)
+    return txt
 
-        # 获取目录信息，然后写文件
-        mulu = get_mulu_from_xml(fn)
-        print(
-            '[%s]%s: mulu file has been written.' % ((datetime.now().strftime('%Y-%m-%d %H:%M:%S')), path.basename(fn)))
-        with open(to_file, 'w') as fp:
-            if mulu:
-                json.dump(mulu, fp, ensure_ascii=False)
+
+def cur_time():
+    return datetime.now().strftime('[%H:%M:%S]')
+
+
+def add_page(index, rows, page_code, juan, line=0):
+    if rows:
+        origin = [format_rare(r) for r in rows]
+        normal = [normalize(r) for r in origin]
+        count = sum(len(r) for r in normal)
+        if count > 15000 or len(origin) > 5000:  # 跳过大文件
+            sys.stderr.write('%s\tfailed:\t%s\t%s lines\t %s chars\tout of limit\n' % (
+                cur_time(), page_code, len(rows), count))
+            return False
+
+        canon_code = book_no = sutra_no = edition = page_no = None  # 藏经代码, 册号，经号，别本, 页码
+        head = re_head_parts.search(page_code)  # ^([A-Z]{1,2})(\d+)n([A-Z]?\d+)([A-Za-z_]?)p([a-z]?\d+)
+        if head:
+            canon_code, book_no, sutra_no, edition, page_no = [head.group(i) for i in range(1, 6)]
+            book_code, sutra_code = canon_code + book_no, canon_code + sutra_no
+        if not juan and index is None:
+            sys.stderr.write('juan not found: %s\n' % page_code)
+
+        try:
+            '''
+            page_code: 页名，由册别、经号、别本、页号组成，例如 A091n1057_p0319
+            canon_code: 藏经代码，例如 A、T
+            book_no: 册号，例如 091
+            book_code: 册别，藏经代码+册号，例如 A091、GA001
+            sutra_no: 经号，例如 1057、A042
+            sutra_code: 典籍编号，藏经代码+经号，例如 A1057
+            page_no: 页号，例如 0319、b005
+            juan: 本页的卷号
+            origin: 原始文本，部分组字式已替换为生僻字
+            normal: 规范文本，是对原始文本的异体字转换为规范字的结果
+            lines: 文本行数
+            char_count: 规范文本(含标点)的字数
+            updated_time: 入库时间
+            '''
+            if index is not None:
+                index(body=dict(
+                    page_code=page_code, canon_code=canon_code, book_no=book_no, book_code=book_code, juan=juan,
+                    sutra_no=sutra_no, sutra_code=sutra_code, edition=edition, page_no=page_no,
+                    origin=origin, normal=normal, lines=len(rows), char_count=count, updated_time=datetime.now())
+                )
             else:
-                fp.write('')
+                output['pages'].append(dict(page_code=page_code, juan=juan, origin=[origin[0], origin[-1]],
+                                            lines=len(rows), char_count=count))
+                if line < 0:
+                    codes = [p['page_code'] for p in output['pages']]
+                    with open('build.log', 'a') as f:
+                        f.write('%s %d pages\n%s\n' % (
+                            book_code, len(output['pages']), ', '.join(codes)))
+                    with open(path.join(path.dirname(__file__), 'build_log', page_code + '.json'), 'w') as f:
+                        json.dump(output['pages'], f, ensure_ascii=False)
+                    output['pages'] = []
+            if line > 0 and 0:
+                sys.stdout.write('%d %s, ' % (line, page_code[len(book_code):]))
+            elif line < 0 and 0:
+                sys.stdout.write(page_code + '\n')
+            return True
+        except ElasticsearchException as e:
+            sys.stderr.write('err %s: %s\n,' % (page_code, str(e)))
+            return False
 
 
-def get_mulu_from_xml(fn):
-    """ 从xml文件中获取目录信息 """
-
-    def fast_iter(context, func):
-        for event, elem in context:
-            func(elem)
-            elem.clear()
-            while elem.getprevious() is not None:
-                del elem.getparent()[0]
-        del context
-
-    def get_mulu(item):
-        if item.xpath('@type') != ['卷']:
-            # 获取行首
-            lb_items = item.xpath('./preceding::*[@ed]')
-            lb = lb_items[-1].xpath('@n') if lb_items else []
-            head = '%s_p%s' % (path.basename(fn).split('.')[0], ','.join(lb))
-            info = {
-                'level': ','.join(item.xpath('@level')),
-                'n': ','.join(item.xpath('@n')),
-                'type': ','.join(item.xpath('@type')),
-                'text': ','.join(item.xpath('text()')),
-                'head': head
-            }
-            mulu.append(info)
-
-    mulu = []
-    context = etree.iterparse(fn, events=('end',), tag='{http://www.cbeta.org/ns/1.0}mulu')
-    fast_iter(context, get_mulu)
-    return mulu
-
-
-def extract_juan_from_xml_p5(source=XML_P5_DIR, overwrite=False):
-    """ 从CBETA xml-p5中提取卷信息 """
-    for fn in glob(path.join(source, '**', '*.xml')):
-        print('[%s]%s: processing... ' % ((datetime.now().strftime('%Y-%m-%d %H:%M:%S')), path.basename(fn)))
-        # 检查卷信息是否已存储
-        head = re.search(r'^([A-Z]{1,2})(\d+)n(.*)', path.basename(fn))
-        assert head and head.group()
-        to_dir = path.join(JUAN_DIR, head.group(1), head.group(1) + head.group(2))
-        if not path.exists(to_dir):
-            os.makedirs(to_dir)
-        to_file = path.join(to_dir, path.splitext(path.basename(fn))[0] + '.json')
-        if not overwrite and path.exists(to_file):
-            print('[%s]%s: juan file existed! ' % ((datetime.now().strftime('%Y-%m-%d %H:%M:%S')), path.basename(fn)))
+def scan_and_index_dir(index, source, book_code):
+    """直接导入目录中的文本数据"""
+    errors = []
+    for i, fn in enumerate(sorted(glob(path.join(source, '**', r'new.txt')))):
+        if book_code and '/%s/' % book_code not in fn:
             continue
+        with open(fn, 'r', encoding='utf-8') as f:
+            lines = f.readlines()
+        rows, page_code = [], None
+        count = idx = total = 0
+        sutra_id = juan = ''
 
-        # 获取卷信息，然后写文件
-        juan = get_juan_from_xml(fn)
-        print(
-            '[%s]%s: juan file has been written.' % ((datetime.now().strftime('%Y-%m-%d %H:%M:%S')), path.basename(fn)))
-        with open(to_file, 'w') as fp:
-            if juan:
-                json.dump(juan, fp, ensure_ascii=False)
+        for row in lines:
+            texts = re.split('#{1,3}', row.strip(), 1)
+            if len(texts) != 2:
+                continue
+            head = re_head_parts.search(texts[0])
+            col_line = head and head.group(6)
+            if head:
+                page_code_ = head.group(0)[:-len(col_line)]
+                if page_code and page_code != page_code_:
+                    count += 1
+                    total += len(rows)
+                    juan = get_juan(page_code) or juan
+                    if not add_page(index, rows, page_code, juan, count):
+                        errors.append(page_code)
+                    rows = []
+                    idx = 0
+                    sutra_id_ = re.sub(r'[A-Za-z_]?p.+$', '', page_code_)
+                    if sutra_id != sutra_id_:
+                        sutra_id = sutra_id_
+                page_code = page_code_
             else:
-                fp.write('')
+                print('head error:\t%s' % row)
+
+            rows.append(junk_filter(texts[1]))
+            idx += 1
+
+        juan = get_juan(page_code) or juan
+        if not add_page(index, rows, page_code, juan, -1):
+            errors.append(page_code)
+        elif rows:
+            count += 1
+            total += len(rows)
+        sys.stdout.write('%s %s %d pages, %d lines\n' % (cur_time(), fn, count, total))
+
+    if errors:
+        with open(path.join(source, datetime.now().strftime('error-%Y%m%d-%H%M.json')), 'w') as f:
+            json.dump(errors, f)
+        print('%s error pages\n%s' % (len(errors), errors))
 
 
-def cmp(juan1, juan2):
-    # n有两种情况，如1/1a
-    if juan1['n'] == juan2['n']:
-        return ['open', 'close'].index(juan1['fun']) - ['open', 'close'].index(juan2['fun'])
-    elif re.sub('[a-z]', '', juan1['n']) == re.sub('[a-z]', '', juan2['n']):
-        return re.sub('[0-9]', '', juan1['n']) > re.sub('[0-9]', '', juan2['n'])
+def build_db(index='cb4ocr-ik', bm_path=BM_PATH, mode='create', book_code='', split='ik'):
+    """ 基于CBETA文本创建索引，以便ocr寻找比对文本使用
+    :param index: 索引名称，为空时可做数据遍历检查，不导入到es库
+    :param bm_path: BM_u8文本目录
+    :param mode: 'create'表示新建，'update'表示更新
+    :param book_code: 仅导入指定册别的页面
+    :param split: 中文分词器的名称，如'ik'或'jieba'
+    """
+    es = index and Elasticsearch(hosts=[config])
+    if not es:
+        return scan_and_index_dir(None, bm_path, book_code)
+
+    if mode == 'create':
+        es.indices.delete(index=index, ignore=[400, 404])
+        es.indices.create(index=index, ignore=400)
     else:
-        return re.sub('[a-z]', '', juan1['n']) > re.sub('[a-z]', '', juan2['n'])
+        es.indices.open(index=index, ignore=400)
 
+    if split == 'ik':
+        mapping = {'properties': {
+            'normal': {'type': 'text', 'analyzer': 'ik_max_word', 'search_analyzer': 'ik_smart'},
+            'origin': {'type': 'text', 'analyzer': 'ik_max_word', 'search_analyzer': 'ik_smart'},
+        }}
+        es.indices.put_mapping(index=index, body=mapping)
+    elif split == 'jieba':
+        mapping = {'properties': {
+            'normal': {'type': 'text', 'analyzer': 'jieba_index', 'search_analyzer': 'jieba_index'},
+            'origin': {'type': 'text', 'analyzer': 'jieba_index', 'search_analyzer': 'jieba_index'},
+        }}
+        es.indices.put_mapping(index=index, body=mapping)
 
-def get_juan_from_xml(fn):
-    """ 从xml文件中获取卷信息 """
-    juan = []
-    root = etree.parse(fn)
-    namespaces = {'cb': 'http://www.cbeta.org/ns/1.0'}
-    for item in root.xpath('//cb:juan', namespaces=namespaces):
-        if item.xpath('@n'):
-            # 获取行首
-            lb_items = item.xpath('./preceding::*[@ed]')
-            lb = lb_items[-1].xpath('@n') if lb_items else []
-            head = '%s_p%s' % (path.basename(fn).split('.')[0], ','.join(lb))
-            juan.append({'n': ','.join(item.xpath('@n')), 'fun': ','.join(item.xpath('@fun')), 'head': head})
-    return juan.sort(key=cmp_to_key(cmp))
-
-
-def order_juan(source=JUAN_DIR):
-    """ 卷信息排序 """
-
-    for fn in glob(path.join(source, '**', '*.json')):
-        print('[%s]%s: processing... ' % ((datetime.now().strftime('%Y-%m-%d %H:%M:%S')), path.basename(fn)))
-        if os.path.getsize(fn) == 0:
-            continue
-        with open(fn, 'r+') as fp:
-            juan_list = json.load(fp)
-            juan_list = [r for r in juan_list if r.get('n')]
-            juan_list.sort(key=cmp_to_key(cmp))
-            fp.seek(0)
-            fp.truncate()
-            json.dump(juan_list, fp)
+    scan_and_index_dir(partial(es.index, index=index, ignore=[]), bm_path, book_code)
 
 
 if __name__ == '__main__':
-    pass
+    import fire
+
+    fire.Fire(build_db)
